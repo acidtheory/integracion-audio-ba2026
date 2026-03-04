bf01e920c7b0bdc096b75b483ea79a183311cf8a
maxy_36 <maxi.morales36@gmail.com>
2026-03-04 20:47:36 -0300

    implementación

--- components/audio_manager.gd
+++ components/audio_manager.gd
@@ -1,6 +1,60 @@
 extends Node
 class_name AudioManager
 
+var music_playing := false
+var distance_from_objective : float 
+var radar_distance_threshold : float = 100
+var radar_playing : bool = false
+
+
+#----------------------------------------------------------------------
+func _ready() -> void:
+	Wwise.register_game_obj(self, "AudioManager")
+	Wwise.load_bank("Main")
+	Wwise.add_default_listener(self)
+	Wwise.set_state("pausa", "pausa_off")
+	start_level_music()
+	
+	#esto stopea xq lo disparo con evento... 
+	
+	#Wwise.set_state("Player_Life", "PL_03")
+
+func start_level_music():
+	if music_playing:
+		Wwise.set_state("Mx_Musica", "GamePlay")
+		
+	else: 
+		Wwise.set_state("Mx_Musica", "GamePlay")
+		Wwise.post_event("Play_Mx_Sw", self)
+		music_playing = true
+#		Wwise.set_rtpc_value("Vol_Mx", 100, self)
+#		Wwise.set_rtpc_value("Vol_Sfx", 50, self)
+		
+		
+
+
+func _process(_delta: float) -> void:
+	check_radar_distance()
+	update_radar_rtpc()
+
+func check_radar_distance():
+	if distance_from_objective <= radar_distance_threshold and not radar_playing:
+		Wwise.post_event("Play_Sfx_Radar", self)
+		radar_playing = true
+
+func update_radar_rtpc():
+	var max_distance = radar_distance_threshold
+	var normalized = clamp(distance_from_objective / max_distance, 0.0, 1.0)
+	var inverted = 1.0 - normalized
+	var rtpc_value = inverted * 100.0
+	
+	Wwise.set_rtpc_value("Sfx_Radar_Vol", rtpc_value, self)
+#func stop_music():
+#	Wwise.stop_all()
+#	music_playing = false
+
+#----------------------------------------------------------------------
+
 # hola maxi, aca te dejo unas variables que se me ocurrieron para el juego,
 # no hace falta que uses todas obvio, pero te queria dar unas cuantas opciones
 # para que juegues con lo que se te vaya ocurriendo
@@ -15,7 +69,7 @@ var direction_to_meteor : Vector2 # un vector normalizado apuntando desde el jug
 
 # lo mismo que arriba pero con los objetivos
 # distancia y direccion al objetivo mas cerca del jugador
-var distance_from_objective : float # distancia en metros hacia el objetivo mas cercano
+#var distance_from_objective : float # distancia en metros hacia el objetivo mas cercano
 var direction_to_objective : Vector2 # un vector normalizado apuntando desde el jugador hacia el objetivo mas cercano
 
 # cuantos objetivos le faltan al jugador
@@ -27,23 +81,35 @@ var objectives_left : int
 var player_is_accelerating : bool
 var player_is_braking : bool
 
+#-----------------------------------------------------------------------------------
 #funciones se llaman cuando pasa algo en especifico, nombres autoexplicativos
 func objective_reached():
+	Wwise.post_event("Play_Sfx_Radar_Fin", self)
+	radar_playing = false
 	print("Objective reached")
 
 func meteor_hit():
+	Wwise.post_event("Play_Sfx_Boton_001", self)
 	print("Meteor hit")
 
 func died():
+	Wwise.set_state("Mx_Musica", "Lose")
+	Wwise.post_event("Play_Sfx_Radar_Fin", self)
 	print("Died")
 
 func win():
+	Wwise.set_state("Mx_Musica", "Win")
+	Wwise.post_event("Play_Sfx_Radar_Fin", self)
 	print("Win")
 
 func paused():
+	Wwise.set_state("pausa", "pausa_on")
+	Wwise.post_event("Play_Sfx_Radar_Fin", self)
 	print("Paused")
 
 func unpaused():
+	Wwise.set_state("pausa", "pausa_off")
+	Wwise.post_event("Play_Sfx_Radar", self)
 	print("Unpaused")
 # me imagine que te pareceria mas comodo pausar y despausar como eventos, pero
 # si lo queres como un booleano, lo podes agarrar con
